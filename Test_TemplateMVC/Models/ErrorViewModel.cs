2b6efad9363571807a0b349baab4835fe89fb31b
yaowaluk yodpanom <yaowamuz.c@gmail.com>
2019-12-12 14:53:37 +0700

    Add project files.

---
+++ Test_TemplateMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Test_TemplateMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
